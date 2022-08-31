54ee2141f8c2e8221b3f1d1ba0e48dd621c2f8db
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-08-31 15:32:54 +0200

    Add ko-papir-ollo

---
+++ exercises/04-ko-papir-ollo.js
@@ -0,0 +1,17 @@
+// Kő - papír - olló játék
+// Írj egy programot, amiben meg lehet adni a játékos1 mutatását, és a játékos2 mutatását, 
+// és a program kiértékeli a menetet, majd kiírja, hogy jatékos1 vagy játékos2 nyert-e.
+
+// Kód vázlat:
+/*
+
+let jatekos1 = 'kő';
+let jatekos2 = 'papír';
+
+...
+
+... console.log('játékos1 nyert!)
+
+...
+
+*/
