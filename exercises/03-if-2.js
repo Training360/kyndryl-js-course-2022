2f087680cb25f45cf3fa7f9e7caae7b3e7f0b05a
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-08-31 15:11:49 +0200

    Add if exercise 2

---
+++ exercises/03-if-2.js
@@ -0,0 +1,15 @@
+// 1.
+// - Hozz létre egy változót, és írd bele a keresztnevedet!
+// - Ha a keresztneved A, B, C vagy D betűvel kezdődik írd ki, hogy "XYZ gyakran felelt először!" 
+//   (ahol XYZ a keresztneved legyen)
+// - Ha a keresztneved Z, Zs betűvel kezdődik, akkor pedig azt írd le, hogy "XYZ ritkán felelt először!"
+// - Ha a keresztneved valamilyen más betűvel kezdődött, akkor írasd ki, hogy "XYZ általában nem felelt először"
+
+// 2.
+// - Hozz létre egy változót, adj neki értéknek 1-999 között (Haladó: random legyen)
+// - Ha a szám nagyobb, mint 500, akkor ellenődizd le, hogy páros-e. Ha igen, akkor írasd ki, hogy 'nagy páros szám'
+//   Ha nem, akkor azt írasd ki, hogy 'kicsi páros szám'
+// - Ha a szám páratlan, akkor csak annyit írj ki, hogy 'páratlan szám'
+
+
+
