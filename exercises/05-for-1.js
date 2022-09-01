a7ec1a4deee4487723e5fb76d4fb927283a31f2b
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-09-01 15:06:00 +0200

    Add halado megoldás to 05-for

--- exercises/05-for-1.js
+++ exercises/05-for-1.js
@@ -64,7 +64,7 @@ for (let i = 0; i < tomb3.length; i++) {
 }
 
 // 4.
-// Hozz létre és írass ki egy tömböt, amiben 100-tól lefelé vannak a 3-mal osztható 
+// Hozz létre és írass ki egy tömböt, amiben 100-tól lefelé vannak a 3-mal osztható
 // természetes számok!
 // (Segítség: ha x % 3 === 0, akkor x osztható hárommal.)
 console.log("4. FELADAT===========================");
@@ -89,11 +89,10 @@ console.log("5. FELADAT===========================");
 let tomb5 = [1.2, 4.23, 6.25, 2.556, 13.3, 74.2, 1.11];
 let tomb5floor = [];
 for (let i = 0; i < tomb5.length; i++) {
-  let lefeleKerekitett = Math.floor(tomb5[i]); 
+  let lefeleKerekitett = Math.floor(tomb5[i]);
   tomb5floor.push(lefeleKerekitett);
 }
 console.log(tomb5floor);
-
 
 // 6. (Haladó)
 // Hozd létre az alábbi tömböt: ['kő', 'papír', 'olló', 'gyík', 'spock']!
@@ -101,7 +100,7 @@ console.log(tomb5floor);
 // az íratódjon ki, hogy 'kő', majd egy következő után pl. az, hogy 'spock'...).
 
 console.log("6. FELADAT===========================");
-let options =  ['kő', 'papír', 'olló', 'gyík', 'spock'];
+let options = ["kő", "papír", "olló", "gyík", "spock"];
 let veletlen0_4 = Math.floor(Math.random() * options.length);
 console.log(options[veletlen0_4]);
 
@@ -109,6 +108,30 @@ console.log(options[veletlen0_4]);
 // Hozz létre egy tömböt, ami a fibonacci sorozat számat tartalmazza a 100-dik elemig!
 // [0, 1, 1, 2, 3, 5, 8, ...]
 // https://hu.wikipedia.org/wiki/Fibonacci-sz%C3%A1mok
+console.log("7. FELADAT===========================");
+let fibonacci = [];
+for (let i = 0; i < 100; i++) {
+  if (i < 2) {
+    fibonacci.push(i);
+  } else {
+    fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
+  }
+}
+console.log(fibonacci);
 
 // 8. (Haladó)
 // Írasd ki az 1000-nél kisebb prím számokat!
+console.log("8. FELADAT===========================");
+let primes = [];
+for (let i = 2; i < 1000; i++) {
+    let osztok = 0;
+    for (let j = 1; j <= i; j++) {
+        if(i % j == 0){
+            osztok++;
+        }
+    }
+    if(osztok === 2){
+        primes.push(i);
+    }
+}
+console.log(primes);
