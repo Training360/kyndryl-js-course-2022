99faada830870190b5b5aec048ded63d2ef0cfbd
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-09-01 13:28:14 +0200

    Add exercises

--- exercises/05-for-1.js
+++ exercises/05-for-1.js
@@ -1,5 +1,10 @@
 // 1.
 // Hozz létre és írass ki egy tömböt, amiben 0-tól 200-ig vannak számok!
+// Segítség:
+// let tomb = [];
+// for (...) {
+//    tomb.push(...)
+// }
 
 // 2.
 // Hozz létre és írass ki egy tömböt, amiben a 0-tól 200-ig található páros számok vannak!
@@ -13,3 +18,27 @@
 // 2 two
 // ...
 
+// 4.
+// Hozz létre és írass ki egy tömböt, amiben 100-tól lefelé vannak a 3-mal osztható számok!
+// (Segítség: ha x % 3 === 0, akkor x osztható hárommal.)
+
+// 5.
+// Hozz létre kézzel egy tömböt, ami nem egész számokat tartalmaz.
+// Pl.: let tomb = [1.2, 4.23, 6.25, 2.556, 13.3, ...];
+// Ezután for ciklus segítségével hozz létre egy olyan tömböt, ami ezeknek a számoknak a
+// lefelé kerekített értékét tartalmazza!
+// pl.: [1, 4, 6, 2, 13]
+// Segítség: Math.floor()
+
+// 6.
+// Hozd létre az alábbi tömböt: ['kő', 'papír', 'olló', 'gyík', 'spock']!
+// Írasd ki a tömb egyik elemét véletlenszerűen! (Tehát pl. az egyik futtatás után
+// az íratódjon ki, hogy 'kő', majd egy következő után pl. az, hogy 'spock'...).
+
+// 7. (Haladó)
+// Hozz létre egy tömböt, ami a fibonacci sorozat számat tartalmazza a 100-dik elemig!
+// [0, 1, 1, 2, 3, 5, 8, ...]
+// https://hu.wikipedia.org/wiki/Fibonacci-sz%C3%A1mok
+
+// 8. (Haladó)
+// Írasd ki az 1000-nél kisebb prím számokat!
