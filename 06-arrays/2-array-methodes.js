6963008dc3d42c1dcfbfd30e4f1c8e171f40be05
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-09-02 14:27:08 +0200

    Add exercises / 08

--- 06-arrays/2-array-methodes.js
+++ 06-arrays/2-array-methodes.js
@@ -1,13 +1,52 @@
-let gyumolcsok = ['alma', 'banán', 'szőlő', 'dinnye'];
+let gyumolcsok = ["alma", "banán", "szőlő", "dinnye"];
 
 // forEach - tömb bejárása függvény segítségével
-console.log("forEach ================")
+console.log("forEach ================");
 
 let nagybetusit = (s) => {
   console.log(s.toUpperCase());
-}
+};
 gyumolcsok.forEach(nagybetusit);
 
 gyumolcsok.forEach((s) => {
   console.log(s.toUpperCase());
 });
+
+gyumolcsok.forEach((s, index) => {
+  console.log(index, s.toUpperCase());
+});
+
+// join - tömb elemek összefűzése
+console.log("join ================");
+let gyumolcsokOsszefuzve = gyumolcsok.join(',');
+console.log(gyumolcsokOsszefuzve);
+
+// concat - tömbök összefűzése (új tömb jön létre)
+console.log("concat ================");
+let osszefuzottTomb = [10, 8, 6].concat([2, 4, 6]);
+console.log(osszefuzottTomb);
+
+// slice - tömb elemek kivágása (új tömb jön létre)
+console.log("slice ================");
+console.log("gyumolcsok:");
+console.log(gyumolcsok);
+console.log("gyumolcsok slice 1-3:");
+console.log(gyumolcsok.slice(1, 3));
+console.log("gyumolcsok slice 2:");
+console.log(gyumolcsok.slice(2));
+
+// splice - tömb elemek eltávolítása (helyben)
+console.log("splice ================");
+let cars = ['Audi', 'BMW', 'Ferrari', 'Mercedes', 'Toyota'];
+console.log(cars);
+cars.splice(2)
+console.log(cars);
+cars = ['Audi', 'BMW', 'Ferrari', 'Mercedes', 'Toyota'];
+cars.splice(2, 1);
+console.log(cars);
+
+// reverse - tömb elemeit fordított sorrenbe rakja
+console.log("reverse ================");
+console.log(gyumolcsok);
+console.log(gyumolcsok.reverse());
+
---
+++ exercises/08-arrays-1.js
@@ -0,0 +1,16 @@
+// 1.
+// Hozz létre egy 5 hosszúságú tömböt, ami 10-nél kisebb számokat tartalmaz!
+// Töröld ki a tömb 3-dik, 4-dik és 5-dik elemét!
+
+console.log("1.FELADAT ============");
+let tomb1 = [ 2, 3, 4, 5, 6 ];
+tomb1.splice(2);
+console.log(tomb1);
+
+// 2.
+// Hozz létre egy 10 elemű tömböt tetszőleges értékekkel!
+// Törölj ki a tömbből minden elemet, ami a második után, és az ötödik elem előtt van, valamint ami a 
+// hatodik után és a kilencedik előtt van.
+
+
+
