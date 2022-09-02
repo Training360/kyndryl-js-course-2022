c99014547e5b4458f68b6edcf65f9c1877eb5985
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-09-02 09:33:29 +0200

    Finished 06-for exercises

--- exercises/06-for-2.js
+++ exercises/06-for-2.js
@@ -21,6 +21,7 @@ users.push({
 
 // 2.
 // Írasd ki az összes user nevét egymás alá!
+console.log("2. FELADAT ================")
 for (let user of users) {
   console.log(user.name);
 }
@@ -29,20 +30,43 @@ for (let user of users) {
 // Írasd ki az összes user id-ját és email címét egy sorba, kettősponttal elválasztva!
 // Pl:
 // 0 : johnd@gmail.com
-// ...
+console.log("3. FELADAT ================")
+for (let user of users) {
+  console.log(user.id + ":" + user.email);
+}
 
 // 4.
 // Írasd ki annak a user-nek az email címét, akinek az id-ja 3!
+console.log("4. FELADAT ================")
+for (let user of users) {
+  if (user.id === 3) {
+    console.log(user.email);
+  }
+}
 
 // 5.
 // Minden user id-ához adj hozzá 1000-et! (Nem elég csak kiíratni)
+console.log("5. FELADAT ================")
 for (let user of users) {
   user.id = user.id + 1000;
 }
 
 // 6.
 // Minden user-nek adj egy active:true property-t!
+console.log("6. FELADAT ================")
+for (let user of users) {
+  user.active = true;
+}
+console.log(users);
 
 // 7.
 // Hozz létre egy új tömböt, amibe csak olyan user-ek kerüljenek be az eddig használt user tömbből,
 // akiknek a neve tartalmaz 'o' betűt!
+console.log("7. FELADAT ================")
+let users2 = [];
+for (let user of users) {
+  if (user.name.includes('o')) {
+    users2.push(user);
+  }
+}
+console.log(users2);
