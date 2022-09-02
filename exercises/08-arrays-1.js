9ff20104471068d5d80c6921e588b259fb8c45b1
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-09-02 15:21:07 +0200

    up exercise 08/2

--- exercises/08-arrays-1.js
+++ exercises/08-arrays-1.js
@@ -3,21 +3,27 @@
 // Töröld ki a tömb 3-dik, 4-dik és 5-dik elemét!
 
 console.log("1.FELADAT ============");
-let tomb1 = [ 2, 3, 4, 5, 6 ];
+let tomb1 = [2, 3, 4, 5, 6];
 tomb1.splice(2);
 console.log(tomb1);
 
 // 2.
 // Hozz létre egy 10 elemű tömböt tetszőleges értékekkel!
-// Törölj ki a tömbből minden elemet, ami a második után, és az ötödik elem előtt van, valamint ami a 
+// Törölj ki a tömbből minden elemet, ami a második után, és az ötödik elem előtt van, valamint ami a
 // hatodik után és a kilencedik előtt van.
+console.log("2.FELADAT ============");
+let tomb2 = [23, 5, 1, 25, 125, 734, 13, 34, 6, 2];
+tomb2.splice(6, 2);
+tomb2.splice(2, 2);
+console.log(tomb2);
+
 
 // 3.
 // Hozz létre 3 db két elemű tömböt, majd fűzd őket össze 1 db 6 elemű tömbbé!
 
 // 4.
 // Írj egy függvényt, ami bemeneti paraméterként két tömböt vár.
-// Ha a tömbök hossza nem egyenlő, akkor térjen vissza a függvény, és írja ki, hogy: 
+// Ha a tömbök hossza nem egyenlő, akkor térjen vissza a függvény, és írja ki, hogy:
 // 'Hiba, nem egyenlő hosszúságú tömbök'.
 // Máskülönben a függvény egy olyan tömbbel térjen vissza, aminek az egyik fele az első paraméterben kapott
 // tömbnek az első fele, a második fele pedig a második paraméterként kapott tömbnek a második fele.
@@ -28,7 +34,7 @@ console.log(tomb1);
 // ['a', 'b', 'g', 'h']
 
 // 5.
-// Írj egy függvényt, ami egy tömböt vár bemeneti paraméterként, és egy stringet. A függvény 
+// Írj egy függvényt, ami egy tömböt vár bemeneti paraméterként, és egy stringet. A függvény
 // soronként írja ki a tömb összes elemét a stringgel összefűzve.
 // Pl.:
 // mergeAll(['peterw', 'johndoe', 'lukesky'], 'gmail.com');
@@ -36,4 +42,3 @@ console.log(tomb1);
 // peterw@gmail.com
 // johndoe@gmail.com
 // lukesky@gmail.com
-
