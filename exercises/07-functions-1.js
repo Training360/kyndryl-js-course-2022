8085f67d376c2544c7887f6e07eb5117b3a66560
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-09-02 10:46:31 +0200

    Add feladat to 07

--- exercises/07-functions-1.js
+++ exercises/07-functions-1.js
@@ -22,10 +22,35 @@ kiirat10szer('alma');
 
 // 3.
 // Írj egy függvényt, ami bemeneti paraméterként 3 értéket vár, és összefűzi ezt a három értéket
-// vesszőkkel elválasztva! Hívd is meg!
+// vesszőkkel elválasztva, majd ezzel az értékkel tér vissza! Hívd is meg!
 // Pl.:
-// osszefuVesszovel('alma', 'körte', 'narancs')
+// console.log(osszefuzVesszovel('alma', 'körte', 'narancs'))
 // Out:
 // alma,körte,narancs
 
 // 4.
+// Írj egy függvényt, ami bemeneti paraméterként egy tömböt vár, és visszaadja a tömb utolsó elemét!
+// Hívd is meg!
+// Pl.:
+// console.log(utolsoElem([1, 2, 3]))
+// Out:
+// 3
+
+// 5.
+// Írj függvényt, ami bemeneti paraméterként egy tömböt és egy számot vár, és térjen vissza
+// a tömbnek azzal az elemével, aminek indexe megegyezik a bementi paraméterként kapott számmal.
+// Ha a bemeneti szám nagyobb, mint amennyi eleme van a tömbnek, akkor írja ki, hogy 'Hiba!' és 0-val
+// térjen vissza.
+// Pl.:
+// console.log(tombIndex([10,20,30], 1));
+// Out:
+// 20
+// Pl .:
+// console.log(tombIndex([10,20,30], 6));
+// Out:
+// Hiba!
+// 0
+
+// 6.
+// 
+
