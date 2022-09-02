7575b155352cb43d117c8eb7b55a54bf83d7260e
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-09-02 15:37:48 +0200

    exercises 08 done

--- exercises/08-arrays-1.js
+++ exercises/08-arrays-1.js
@@ -17,28 +17,58 @@ tomb2.splice(6, 2);
 tomb2.splice(2, 2);
 console.log(tomb2);
 
-
 // 3.
 // Hozz létre 3 db két elemű tömböt, majd fűzd őket össze 1 db 6 elemű tömbbé!
+console.log("3.FELADAT ============");
+let t1 = [1, 2, 3];
+let t2 = [4, 5, 6];
+let t3 = [7, 8, 9];
+
+let result = t1.concat(t2).concat(t3);
+// result = result.concat(t3);
+console.log(result);
 
 // 4.
 // Írj egy függvényt, ami bemeneti paraméterként két tömböt vár.
 // Ha a tömbök hossza nem egyenlő, akkor térjen vissza a függvény, és írja ki, hogy:
 // 'Hiba, nem egyenlő hosszúságú tömbök'.
-// Máskülönben a függvény egy olyan tömbbel térjen vissza, aminek az egyik fele az első paraméterben kapott
-// tömbnek az első fele, a második fele pedig a második paraméterként kapott tömbnek a második fele.
+// Máskülönben a függvény egy olyan tömbbel térjen vissza, aminek az egyik fele az első paraméterben
+// kapott tömbnek az első fele, a második fele pedig a második paraméterként kapott tömbnek a
+// második fele.
 // Páratlan tömbhossz esetén lefele kerekítünk.
 // Pl.:
 // console.log(merge(['a', 'b', 'c', 'd'], ['e', 'f', 'g', 'h']))
 // Out:
 // ['a', 'b', 'g', 'h']
+console.log("4.FELADAT ============");
+function merge(t1, t2) {
+  if (t1.length !== t2.length) {
+    console.log("Hiba, nem egyenlő hosszúságú tömbök!");
+    return;
+  }
+
+  let cutIndex = Math.floor(t1.length / 2);
+  let t1firstHalf = t1.slice(0, cutIndex);
+  let t2secondHalf = t2.slice(cutIndex);
+  return t1firstHalf.concat(t2secondHalf);
+}
+console.log(merge(["a", "b", "c", "d"], ["e", "f", "g", "h"]));
+console.log(merge(["a", "b", "c", "d", "e"], [1, 2, 3, 4, 5]));
 
 // 5.
 // Írj egy függvényt, ami egy tömböt vár bemeneti paraméterként, és egy stringet. A függvény
 // soronként írja ki a tömb összes elemét a stringgel összefűzve.
 // Pl.:
-// mergeAll(['peterw', 'johndoe', 'lukesky'], 'gmail.com');
+// mergeAll(['peterw', 'johndoe', 'lukesky'], '@gmail.com');
 // Out:
 // peterw@gmail.com
 // johndoe@gmail.com
 // lukesky@gmail.com
+console.log("5.FELADAT ============");
+
+function mergeAll(arr, word) {
+  arr.forEach((e) => {
+    console.log(e + word);
+  });
+}
+mergeAll(["peterw", "johndoe", "lukesky"], "@gmail.com");
