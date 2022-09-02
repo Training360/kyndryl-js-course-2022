f4873a86e00c7b55a318c71907373fbe52d69944
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-09-02 14:38:16 +0200

    up exercise 08

--- exercises/08-arrays-1.js
+++ exercises/08-arrays-1.js
@@ -12,5 +12,28 @@ console.log(tomb1);
 // Törölj ki a tömbből minden elemet, ami a második után, és az ötödik elem előtt van, valamint ami a 
 // hatodik után és a kilencedik előtt van.
 
+// 3.
+// Hozz létre 3 db két elemű tömböt, majd fűzd őket össze 1 db 6 elemű tömbbé!
 
+// 4.
+// Írj egy függvényt, ami bemeneti paraméterként két tömböt vár.
+// Ha a tömbök hossza nem egyenlő, akkor térjen vissza a függvény, és írja ki, hogy: 
+// 'Hiba, nem egyenlő hosszúságú tömbök'.
+// Máskülönben a függvény egy olyan tömbbel térjen vissza, aminek az egyik fele az első paraméterben kapott
+// tömbnek az első fele, a második fele pedig a második paraméterként kapott tömbnek a második fele.
+// Páratlan tömbhossz esetén lefele kerekítünk.
+// Pl.:
+// console.log(merge(['a', 'b', 'c', 'd'], ['e', 'f', 'g', 'h']))
+// Out:
+// ['a', 'b', 'g', 'h']
+
+// 5.
+// Írj egy függvényt, ami egy tömböt vár bemeneti paraméterként, és egy stringet. A függvény 
+// soronként írja ki a tömb összes elemét a stringgel összefűzve.
+// Pl.:
+// mergeAll(['peterw', 'johndoe', 'lukesky'], 'gmail.com');
+// Out:
+// peterw@gmail.com
+// johndoe@gmail.com
+// lukesky@gmail.com
 
