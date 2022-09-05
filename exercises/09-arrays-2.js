6ca3bf5fb389d8191d3e50c701ddb6eda4820bcb
vblaskovics <viktor.blaskovics@gmail.com>
2022-09-05 10:55:45 +0200

    solution exercise 09

--- exercises/09-arrays-2.js
+++ exercises/09-arrays-2.js
@@ -1,12 +1,25 @@
 let students = [
-  {firstname: 'Peter', lastname: 'White', age: 12},
-  {firstname: 'Peter', lastname: 'Black', age: 14},
-  {firstname: 'Ben', lastname: 'Green', age: 8},
-  {firstname: 'George', lastname: 'Red', age: 10},
-  {firstname: 'John', lastname: 'Yellow', age: 11}
-]
+  { firstname: "Peter", lastname: "White", age: 12 },
+  { firstname: "Peter", lastname: "Black", age: 14 },
+  { firstname: "Ben", lastname: "Green", age: 8 },
+  { firstname: "George", lastname: "Red", age: 10 },
+  { firstname: "John", lastname: "Yellow", age: 11 },
+];
 
 // FELADAT
-// Hozz létre egy tömböt a 10 és 12 év közötti tanulók teljes nevével, 
+// Hozz létre egy tömböt a 10 és 12 év közötti tanulók teljes nevével,
 // ABC sorrend szerint növekedve!
-// Használd a sort, filter és map függvényeket 
+// Használd a sort, filter és map függvényeket
+
+let students2 = students
+  .filter((s) => 10 <= s.age && s.age <= 12)
+  .map((s) => s.firstname + ' ' + s.lastname)
+  .sort((name1, name2) => {
+    if(name1 < name2) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
+console.log(students2);
